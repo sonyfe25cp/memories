bcb7e2b56f06e9928834786d985116607becdd66
Sonyfe25cp <sonyfe25cp@gmail.com>
2016-06-17 15:38:19 +0800

    update cr

---
+++ corporate-reputation-structure.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1466128169241" ID="ID_953455267" MODIFIED="1466128192846" TEXT="&#x4f01;&#x4e1a;&#x58f0;&#x8a89;"/>
+</map>
